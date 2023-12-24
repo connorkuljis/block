03efc64e7e7a0fbc861c638ddc8f3f6d808f6dbc
Connor <conkuljis@gmail.com>
2023-12-25 00:45:38 +0800

    created goroutines to pause/unpause timer on keypress

--- go.mod
+++ go.mod
@@ -2,12 +2,18 @@ module github.com/connorkuljis/loop-twice
 
 go 1.21.1
 
-require github.com/briandowns/spinner v1.23.0
+require (
+	github.com/briandowns/spinner v1.23.0
+	github.com/schollz/progressbar/v3 v3.14.1
+)
 
 require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
 	github.com/fatih/color v1.7.0 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/mattn/go-isatty v0.0.8 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	golang.org/x/term v0.1.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,16 +2,97 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
-	"github.com/briandowns/spinner"
+	"github.com/eiannone/keyboard"
+	"github.com/schollz/progressbar/v3"
 )
 
+func worker(pauseCh, stopCh chan bool, wg *sync.WaitGroup) {
+	n := 100
+
+	bar := progressbar.Default(int64(n))
+
+	for i := 0; i < n; i++ {
+		select {
+		case <-pauseCh:
+			// The goroutine will be "suspended" until a resume signal is received
+			fmt.Println("Paused. Waiting to be resumed.")
+			<-pauseCh
+
+		case <-stopCh:
+			fmt.Println("Exiting.")
+			wg.Done()
+			return
+
+		default:
+			bar.Add(1)
+			time.Sleep(40 * time.Millisecond)
+		}
+
+	}
+	fmt.Println("worker done.")
+	wg.Done()
+}
+
+func printKeyPressed(pauseCh, stopCh chan bool) {
+	for {
+		select {
+
+		case <-stopCh:
+			fmt.Printf("stopped looking for key.")
+			return
+
+		default:
+			char, key, err := keyboard.GetKey()
+			if err != nil {
+				fmt.Println("error: " + err.Error())
+				panic(err)
+			}
+
+			if key == keyboard.KeyCtrlC {
+				fmt.Println("Ctrl+C pressed. Exiting.")
+				return
+			}
+
+			fmt.Printf("Key pressed: %c\n", char)
+
+			pauseCh <- true
+		}
+	}
+}
+
 func main() {
-	s := spinner.New(spinner.CharSets[39], 100*time.Millisecond)
-	s.Start()
-	time.Sleep(4 * time.Second)
-	s.Stop()
+	err := keyboard.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	pauseCh := make(chan bool)
+	stopCh := make(chan bool)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go worker(pauseCh, stopCh, &wg)
+	go printKeyPressed(pauseCh, stopCh)
+
+	wg.Wait()
+
+	// keyboard.Close()
+	fmt.Println("Goodbye.")
+}
+
+func progressBarDemo() {
+	bar := progressbar.Default(100)
+	for i := 0; i < 100; i++ {
+		bar.Add(1)
+		time.Sleep(40 * time.Millisecond)
+	}
+}
+
+func spinnerDemo() {
 }
 
 func loop() {
