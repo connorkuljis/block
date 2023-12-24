d8a29f5f491499e26ad8d50c00640d38bc000311
Connor <conkuljis@gmail.com>
2023-12-25 02:12:14 +0800

    working but deadlocks if progress bar is sleeping

--- main.go
+++ main.go
@@ -9,60 +9,6 @@ import (
 	"github.com/schollz/progressbar/v3"
 )
 
-func worker(pauseCh, stopCh chan bool, wg *sync.WaitGroup) {
-	n := 100
-
-	bar := progressbar.Default(int64(n))
-
-	for i := 0; i < n; i++ {
-		select {
-		case <-pauseCh:
-			// The goroutine will be "suspended" until a resume signal is received
-			fmt.Println("Paused. Waiting to be resumed.")
-			<-pauseCh
-
-		case <-stopCh:
-			fmt.Println("Exiting.")
-			wg.Done()
-			return
-
-		default:
-			bar.Add(1)
-			time.Sleep(40 * time.Millisecond)
-		}
-
-	}
-	fmt.Println("worker done.")
-	wg.Done()
-}
-
-func printKeyPressed(pauseCh, stopCh chan bool) {
-	for {
-		select {
-
-		case <-stopCh:
-			fmt.Printf("stopped looking for key.")
-			return
-
-		default:
-			char, key, err := keyboard.GetKey()
-			if err != nil {
-				fmt.Println("error: " + err.Error())
-				panic(err)
-			}
-
-			if key == keyboard.KeyCtrlC {
-				fmt.Println("Ctrl+C pressed. Exiting.")
-				return
-			}
-
-			fmt.Printf("Key pressed: %c\n", char)
-
-			pauseCh <- true
-		}
-	}
-}
-
 func main() {
 	err := keyboard.Open()
 	if err != nil {
@@ -72,38 +18,61 @@ func main() {
 	pauseCh := make(chan bool)
 	stopCh := make(chan bool)
 
+	// synchronise progress bar
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	go worker(pauseCh, stopCh, &wg)
-	go printKeyPressed(pauseCh, stopCh)
+	go progressBarWorker(pauseCh, stopCh, &wg)
+	go checkKeys(pauseCh, stopCh)
 
+	// wait for progress bar to complete
 	wg.Wait()
 
-	// keyboard.Close()
-	fmt.Println("Goodbye.")
+	stopCh <- true // signal keychecker to exit
+	keyboard.Close()
 }
 
-func progressBarDemo() {
-	bar := progressbar.Default(100)
-	for i := 0; i < 100; i++ {
-		bar.Add(1)
-		time.Sleep(40 * time.Millisecond)
-	}
-}
+func progressBarWorker(pauseCh, stopCh chan bool, wg *sync.WaitGroup) {
+	n := 100
+	bar := progressbar.Default(int64(n))
 
-func spinnerDemo() {
-}
-
-func loop() {
-	max, iter := 255, 2
-	for i := 0; i < iter; i++ {
-		iterateNums(max)
-	}
-}
-
-func iterateNums(n int) {
 	for i := 0; i < n; i++ {
-		fmt.Printf("%d\t", i)
+		select {
+		case <-pauseCh:
+			<-pauseCh // sleeps until signal
+		case <-stopCh:
+			wg.Done()
+			return
+		default:
+			bar.Add(1)
+			time.Sleep(40 * time.Millisecond)
+		}
+	}
+
+	wg.Done()
+}
+
+func checkKeys(pauseCh, stopCh chan bool) {
+	keysEvents, err := keyboard.GetKeys(10)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		select {
+		case <-stopCh:
+			fmt.Println("**Exiting keys thread.")
+			return
+		case event := <-keysEvents:
+			if event.Err != nil {
+				panic(event.Err)
+			}
+			fmt.Printf("You pressed: rune %q, key %X\r\n", event.Rune, event.Key)
+			if event.Key == keyboard.KeyEsc {
+				stopCh <- true
+			} else {
+				pauseCh <- true
+			}
+		}
 	}
 }
